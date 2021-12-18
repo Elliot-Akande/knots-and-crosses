b4ed188f19f4f537122f365f2af5c07dfa5e8827
Elliot Akande <elbo.lynch@gmail.com>
2021-12-18 04:15:20 +0000

    Add gameover detection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const board = (() => {
+const gameBoard = (() => {
     let _board = ["", "", "", "", "", "", "", "", "",];
 
     const _validPosition = position => position >= 0 && position < 9;
@@ -30,14 +30,33 @@ const playerFactory = (symbol) => {
 };
 
 const engine = ((playerOne, playerTwo) => {
+    const _symbols = [playerOne.getSymbol(), playerTwo.getSymbol()];
     let _currentTurn = playerOne;
 
     const _nextTurn = () => _currentTurn == playerOne ? _currentTurn = playerTwo : _currentTurn = playerOne;
     const _getPosition = e => e.srcElement.getAttribute("data-position");
+    const _checkWin = () => {
+        const board = gameBoard.get();
+        let winner = "";
+
+        _symbols.forEach(symbol => {
+            if ((board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
+                (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
+                (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
+                (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
+                (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
+                (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
+                (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
+                (board[2] == symbol && board[4] == symbol && board[6] == symbol)) 
+                winner = symbol;
+        });
+        return winner ? winner : false;
+    };
 
     const playTurn = e => {
         const position = _getPosition(e);
-        if (board.add(getTurn(), position)) _nextTurn();
+        if (gameBoard.add(getTurn(), position)) _nextTurn();
+        if (_checkWin()) displayController.gameOver();
     }
     const getTurn = () => _currentTurn.getSymbol();
 
@@ -54,10 +73,12 @@ const displayController = (() => {
     const _segmentPressed = e => engine.playTurn(e);
 
     const update = (symbol, position) => document.querySelector(`[data-position="${position}"]`).innerText = symbol;
+    const gameOver = () => _segments.forEach(segment => segment.disabled = true);
 
     _segments.forEach(segment => segment.addEventListener("click", _segmentPressed));
 
     return {
-        update
+        update,
+        gameOver
     };
 })();
